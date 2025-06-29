dd96ca948b8677a4533c15500df706fc5bb15f31
kishlay <kishlay141@gmail.com>
2025-06-29 12:46:16 +0530

    chore: initialize Next.js project with essential dependencies and Tailwind CSS setup

---
+++ just-lputhinks/tailwind.config.ts
@@ -0,0 +1,16 @@
+import forms from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [forms],
+};
+
+export default config;

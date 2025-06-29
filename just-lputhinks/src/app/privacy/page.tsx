e24e0efa4f96ffa8701253a02b70c5fc36149a85
kishlay <kishlay141@gmail.com>
2025-06-29 16:59:40 +0530

    fix: added  minimal placeholders to all empty files, was causing error while deploying lol

---
+++ just-lputhinks/src/app/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default function FeedbackPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-semibold">Privacy Page</h1>
+      <p>This is a placeholder page for now.</p>
+    </div>
+  );
+}
---
+++ just-lputhinks/src/app/resources/[semester]/page.tsx
@@ -0,0 +1,8 @@
+export default function FeedbackPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-semibold">Semster Page</h1>
+      <p>This is a placeholder page for now.</p>
+    </div>
+  );
+}
